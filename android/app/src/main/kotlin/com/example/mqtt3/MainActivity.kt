46b13ac9b468ec70c6ec0296178254bded3c0c33
The 1975 <juh5032@konkuk.ac.kr>
2024-07-17 22:17:03 +0900

    ver1

---
+++ android/app/src/main/kotlin/com/example/mqtt3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mqtt3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter MQTT Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final client = MqttServerClient('3.64.90.245', '');
+
+  @override
+  void initState() {
+    super.initState();
+    _connect();
+  }
+
+  Future<void> _connect() async {
+    client.port = 1883;
+    client.logging(on: true);
+    client.keepAlivePeriod = 20;
+    client.onConnected = _onConnected;
+    client.onDisconnected = _onDisconnected;
+    client.onSubscribed = _onSubscribed;
+    client.onSubscribeFail = _onSubscribeFail;
+    client.pongCallback = _pong;
+
+    final connMessage = MqttConnectMessage()
+        .withClientIdentifier('THE_1975_')
+        .withWillTopic('willtopic')
+        .withWillMessage('My Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+
+    client.connectionMessage = connMessage;
+
+    try {
+      await client.connect();
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    _subscribeToTopic('test/zini_hyeon');
+  }
+
+  void _onConnected() {
+    print('Connected');
+  }
+
+  void _onDisconnected() {
+    print('Disconnected');
+  }
+
+  void _onSubscribed(String topic) {
+    print('Subscribed to $topic');
+  }
+
+  void _onSubscribeFail(String topic) {
+    print('Failed to subscribe $topic');
+  }
+
+  void _pong() {
+    print('Ping response client callback invoked');
+  }
+
+  void _subscribeToTopic(String topic) {
+    client.subscribe(topic, MqttQos.atLeastOnce);
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>> c) {
+      final MqttPublishMessage message = c[0].payload as MqttPublishMessage;
+      final payload =
+          MqttPublishPayload.bytesToStringAsString(message.payload.message);
+
+      print('Received message:$payload from topic: ${c[0].topic}>');
+    });
+  }
+
+  void _publishMessage(String topic, String message) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+
+    client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter MQTT Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                _publishMessage('test/zini_hyeon', 'LETs_GO_TEAM_WALLMARIA');
+              },
+              child: Text('Publish Message'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    client.disconnect();
+    super.dispose();
+  }
+}
